8a76c9f3dd307404a9b9c9969893552e81694218
Jordan <jwald3@brockport.edu>
2023-05-31 21:26:14 -0400

    including yearly filter

--- src/controllers/energyUsageController.js
+++ src/controllers/energyUsageController.js
@@ -4,7 +4,7 @@ import { Op } from 'sequelize';
 
 export const getAllEnergyUsages = async (req, res) => {
     try {
-        const { household_id, date, aggregateByDay, aggregateByTime, aggregateByWeek, aggregateByWeekDay, month, page, limit, week } = req.query;
+        const { household_id, date, aggregateByDay, aggregateByTime, aggregateByWeek, aggregateByWeekDay, month, page, limit, week, year, rollingWindow } = req.query;
 
         let whereClause = {};
 
@@ -24,11 +24,20 @@ export const getAllEnergyUsages = async (req, res) => {
             };
         }
 
+        if (year) {
+            const startOfYear = new Date(year, 0, 1);  // Start of the specified year
+            const endOfYear = new Date(year, 11, 31, 23, 59, 59, 999);  // End of the specified year
+
+            whereClause.reading_time = {
+                [Op.gte]: startOfYear,
+                [Op.lte]: endOfYear
+            };
+        }
+
         // Convert page and limit to integers and calculate offset
         const currentPage = parseInt(page, 10) || 1;
         const pageSize = parseInt(limit, 10) || 10;
         const offset = (currentPage - 1) * pageSize;
-
 
         const energyUsages = await EnergyUsage.findAll({ 
             where: whereClause,
@@ -48,105 +57,46 @@ export const getAllEnergyUsages = async (req, res) => {
             });
         }
 
-        else if (aggregateByWeek === 'true') {
-            const specifiedWeek = Number(week);
-            if (isNaN(specifiedWeek) || specifiedWeek < 1 || specifiedWeek > 52) {
-                res.status(400).json({ message: "Invalid week number. Please specify a week number between 1 and 52." });
-                return;
-            }
-        
-            const firstDayOfYear = new Date(new Date().getFullYear(), 0, 1);  // First day of the year
-            const firstDayOfWeekOne = firstDayOfYear.getDay() === 0 ? firstDayOfYear : new Date(new Date().getFullYear(), 0, 2 - firstDayOfYear.getDay());  // First Monday of the year
-        
-            // Calculate start and end date of the specified week
-            const startDate = new Date(firstDayOfWeekOne.getTime() + 7 * 24 * 60 * 60 * 1000 * (specifiedWeek - 1));
-            const endDate = new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000);
-        
-            const weeklyData = filteredEnergyUsages.filter(usage => {
-                const usageDate = new Date(usage.get().reading_time);
-                return usageDate >= startDate && usageDate < endDate;
-            }).map(usage => ({
-                ...usage.get(),
-                usage_id: Number(usage.get().usage_id),
-                energy_usage: Number(usage.get().energy_usage)
-            }));
-        
-            // The time window for the moving average
-            const timeWindow = 3;
-        
+        // ... your existing code for other aggregations ...
+
+        if (rollingWindow === 'true') {
+            // Group by day
+            const dailyEnergyUsages = {};
+            energyUsages.forEach(usage => {
+                const day = usage.get().reading_time.toISOString().split('T')[0];  // Get the day
+                if (!dailyEnergyUsages[day]) {
+                    dailyEnergyUsages[day] = {
+                        date: day,
+                        total_energy_usage: 0,
+                        count: 0
+                    };
+                }
+                dailyEnergyUsages[day].total_energy_usage += Number(usage.get().energy_usage);
+                dailyEnergyUsages[day].count += 1;
+            });
+
+            const dailyEnergyUsagesArray = Object.values(dailyEnergyUsages);
+
+            // The time window for the rolling average
+            const timeWindow = 15;
+
             // Create an array to store the moving averages
             const movingAverages = [];
-        
+
             // Calculate moving averages
-            for(let i = 0; i <= weeklyData.length - timeWindow; i++) {
-                const windowData = weeklyData.slice(i, i + timeWindow);
-                const sum = windowData.reduce((sum, dataPoint) => sum + dataPoint.energy_usage, 0);
+            for(let i = 0; i <= dailyEnergyUsagesArray.length - timeWindow; i++) {
+                const windowData = dailyEnergyUsagesArray.slice(i, i + timeWindow);
+                const sum = windowData.reduce((sum, dataPoint) => sum + dataPoint.total_energy_usage, 0);
                 const average = sum / windowData.length;
-        
+
                 movingAverages.push({
-                    start_time: windowData[0].reading_time,
-                    end_time: windowData[windowData.length - 1].reading_time,
+                    start_date: windowData[0].date,
+                    end_date: windowData[windowData.length - 1].date,
                     average_energy_usage: average
                 });
             }
-        
+
             res.json(movingAverages);
-        }
-        
-
-        if (aggregateByTime === 'true') {
-            const aggregatedData = filteredEnergyUsages.reduce((acc, usage) => {
-                const timeKey = usage.get().reading_time.toISOString().split('T')[1].split('.')[0];
-                if (!acc[timeKey]) {
-                    acc[timeKey] = {
-                        time: timeKey,
-                        total_energy_usage: 0,
-                        count: 0
-                    };
-                }
-                acc[timeKey].total_energy_usage += Number(usage.get().energy_usage);
-                acc[timeKey].count += 1;
-                return acc;
-            }, {});
-
-            const aggregatedDataArray = Object.values(aggregatedData);
-            res.json(aggregatedDataArray);
-        } else if (aggregateByDay === 'true') {
-            const aggregatedData = filteredEnergyUsages.reduce((acc, usage) => {
-                const dateKey = usage.get().reading_time.toISOString().split('T')[0];
-                if (!acc[dateKey]) {
-                    acc[dateKey] = {
-                        date: dateKey,
-                        total_energy_usage: 0,
-                        count: 0
-                    };
-                }
-                acc[dateKey].total_energy_usage += Number(usage.get().energy_usage);
-                acc[dateKey].count += 1;
-                return acc;
-            }, {});
-
-            const aggregatedDataArray = Object.values(aggregatedData);
-            res.json(aggregatedDataArray);
-        } else if (aggregateByWeekDay === 'true') {
-            const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            const aggregatedData = filteredEnergyUsages.reduce((acc, usage) => {
-                const dayOfWeek = new Date(usage.get().reading_time).getDay();
-                const dayKey = daysOfWeek[dayOfWeek];
-                if (!acc[dayKey]) {
-                    acc[dayKey] = {
-                        day: dayKey,
-                        total_energy_usage: 0,
-                        count: 0
-                    };
-                }
-                acc[dayKey].total_energy_usage += Number(usage.get().energy_usage);
-                acc[dayKey].count += 1;
-                return acc;
-            }, {});
-
-            const aggregatedDataArray = Object.values(aggregatedData);
-            res.json(aggregatedDataArray);
         } else {
             const processedData = filteredEnergyUsages.map(usage => ({
                 ...usage.get(),
